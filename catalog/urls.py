eaea04c17541a742388b4faa0be21864eb6d3c56
ryanchee27 <cheery@dukes.jmu.edu>
2025-11-11 01:45:56 -0500

    lab11

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,3 +10,17 @@ urlpatterns = [
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    path('borrowed/', views.LoanedBooksByUserListView.as_view(), name='all-borrowed'),
+]
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
